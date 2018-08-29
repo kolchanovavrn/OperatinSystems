e14692f56ea2cfc426e7503d3d97bdacdfdc6531
root <root@DESKTOP-7DAURTA.localdomain>
2018-08-29 14:02:15 +0300

    first commit

---
+++ week2/task1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+int main()
+{
+    printf("Integer max = %f \n ", INT_MAX);
+    printf("Float max = %f \n", FLT_MAX);
+    printf("Double max = %f \n ", DBL_MAX);
+    return 0;
+}
---
+++ week2/task2_temp.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[]="Hello world";
+    printf("%s \n", str);
+    t str_len = strlen(str);
+    printf("%u \n", str_len);
+    while 
+
+return (0);
+}
+
