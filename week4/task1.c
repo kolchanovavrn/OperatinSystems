f18381170ff03734ead18f7ed5d8d6f99da2eeb0
kolchanovavrn <root@DESKTOP-7DAURTA.localdomain>
2018-09-12 12:55:19 +0300

    task1 is fixed

--- week4/task1.c
+++ week4/task1.c
@@ -2,16 +2,28 @@
 #include <stdio.h>
 #include <sys/types.h>
 
-#define N 15
+#define N 1
 int main(){
   int i, PID;
     
-  fork();
-  
-  PID = getpid();
+  /*fork();*/
+  /*PID = getpid();*/
 
-  printf("Hello from parent %d \n", PID - N);
+  PID = fork();
+  if(PID > 0) {
+        /*parent logic*/
+        printf("Hello from parent %d \n", PID - N);
+
+    } else if (PID == 0) {
+        printf ("Hello from child %d \n", PID - N);
+        /*child logic*/
+    } else {
+        printf("Error! PID =  %d ", PID - N);
+        /*error*/
+    }
+  /*printf("Hello from parent %d \n", PID - N);
   printf("Hello from child %d \n", PID - N);
-  
+  */
+
   return 0;
 }
