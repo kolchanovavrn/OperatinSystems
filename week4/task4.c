f790a16891f47249d7fc4749048ea7d9782a57dd
kolchanovavrn <root@DESKTOP-7DAURTA.localdomain>
2018-09-12 21:18:14 +0300

    task4 draft is added

--- week4/task4.c
+++ week4/task4.c
@@ -3,14 +3,27 @@
 #include <unistd.h>
 #include <stdio.h>
 
+#define size 124
 int main(){
     
-    char input[25];
-    
-    while(1){
-        printf("$ ");:       
-        system(fgets(input,20,stdin));
+    char input[size];
+    int pid;
+    int i = 0;
+    int k = 0;
 
+    while(1){
+        printf("-> ");      
+        fgets(input,size - 1, stdin);
+        while (input[k] != '\n')
+            k++;
+
+        if (input[k-1] != '&'){
+            system(input);
+        }else {
+            input[k-1] = '\0';
+            pid = fork();
+            system(input);
+        }
     }
     return 0;
 }
