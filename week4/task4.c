c3a55d09a5e0e74f813731b5825dacc60583df07
kolchanovavrn <root@DESKTOP-7DAURTA.localdomain>
2018-09-12 19:05:25 +0300

    draft for task4 is added

---
+++ week4/task4.c
@@ -0,0 +1,16 @@
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(){
+    
+    char input[25];
+    
+    while(1){
+        printf("$ ");:       
+        system(fgets(input,20,stdin));
+
+    }
+    return 0;
+}
