e9cf99737d7459f53f9d002a8890d9654415178a
kolchanovavrn <root@DESKTOP-7DAURTA.localdomain>
2018-09-26 22:35:46 +0300

    Task 5 is added

---
+++ week6/task5.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void handler(int signal_no){
+    switch(signal_no){
+        case SIGINT:{
+            printf("Get sigint! \n");
+            break;
+       }
+       case SIGKILL:{
+            printf("Get sigkill! \n");
+            break;
+                    }
+       case SIGSTOP:{
+            printf("Get sigstop! \n");        
+            break;
+                    }
+       case SIGUSR1:{
+            printf("Get sigusr1! \n");
+            break;
+                    }
+       default:{
+            printf("Get default! :( \n");   
+            break;
+               }
+    }
+    exit(0);
+}
+
+int main(){
+    printf("Start... \n");
+    signal(SIGINT, handler);
+    signal(SIGKILL, handler);
+    signal(SIGSTOP, handler);
+    signal(SIGUSR1, handler);
+    
+    pid_t child = fork();
+    if (child){
+        sleep(10);
+        kill(child, SIGTERM);
+    }else{
+        while (1){
+            printf("I'm alive... \n");
+            sleep(1);
+        }
+    }
+    
+
+    return 0;
+}
