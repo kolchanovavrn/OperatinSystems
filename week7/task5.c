1ea7f864bea46ecbe39f8278f280a455635a8fe9
kolchanovavrn <root@DESKTOP-7DAURTA.localdomain>
2018-10-05 00:15:14 +0300

    task5 is added

---
+++ week7/task5.c
@@ -0,0 +1,16 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <stdio.h> 
+int main() { 
+    char **s = malloc(0);
+    char foo[] = "Hello World"; 
+    *s = foo;
+    printf("s is %s\n", *s);
+    s[0] = foo;
+    printf("s[0] is %s\n", s[0]); 
+    return(0); 
+} 
