19ccfaef54a8902f0d3ab4f0b4c7724651452198
kolchanovavrn <root@DESKTOP-7DAURTA.localdomain>
2018-10-26 17:02:43 +0300

    task4 is changed

--- week10/ex4.c
+++ week10/ex4.c
@@ -3,19 +3,28 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <dirent.h>
 
 int main(){
     
-    DIR *dirp = opendir("./tmp");
-    struct dirent *dp;
-    struct stat  *buffer = malloc(sizeof(struct stat)); 
+    FILE *fp;
+    fp = fopen("ex4.txt", "w");
+
+    DIR* dirp = opendir("tmp");
+    
+    char path[50];
+    struct dirent* dirEntry;
+    struct stat buffer; 
     
     int amount_of_files = 0;
     int index = 0;
 
-    while ( (dp = readdir(dirp)) != NULL){
-        stat(dp.d_name,buffer);
-        if (*(buffer).st_nlink >=2){
+    while ( (dirEntry = readdir(dirp)) != NULL){
+        strcpy(path, "tmp/");
+        strcat(path, dirEntry->d_name); //constructing a path - tmp/name_of_entry
+        stat(path,&buffer);
+
+        if (buffer.st_nlink >=2){
             amount_of_files++;
         }
     }    
@@ -24,39 +33,39 @@ int main(){
     struct dirent dfiles[amount_of_files];
 
     dirp = opendir("./tmp");
-    while ( (dp = readdir(dirp)) !=NULL ) {
-        //идем по файлам в папке
-       
-        stat(dp,buffer);
-        if (buffer->st_nlink >= 2){
-            files[index] = *buffer;
-            dfiles[index] = *dp;
+    while ( (dirEntry = readdir(dirp)) !=NULL ) {     
+        stat(path,&buffer);
+        if (buffer.st_nlink >= 2){
+            files[index] = buffer;
+            dfiles[index] = *dirEntry;
             index++;
         }
     }
+    
+    int delta = 0;
+    int inode_num = 0;
+    int curr = 0;
+    int temp = 0;
 
     for (int i = 0; i<amount_of_files; i++){
         inode_num = files[i].st_ino;
         delta = amount_of_files - i -1;
         for (int j = 1; j < delta; j++){
-            
+
             curr = files[i+j].st_ino;
             
             if (curr = 0) continue;
-            print(curr);
+            fprintf(fp, "%s", curr);
+            
             if (inode_num == temp){
-                printf(dfiles[i+j]->d_name);
+                fprintf(fp, "%s", dfiles[i+j].d_name);
             }
-              
             
-            files[i_j].st_ino = 0;
-            //jnode_num = files[j].st_ino;
-            
-
-
+            files[i+j].st_ino = 0;           
         }
-    
+        fprintf(fp, "%s", "\n");
     }
 
+    fclose(fp);
     return 0;
 }
